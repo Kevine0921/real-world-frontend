4e5258b9fc44a99cb8ea34f398be507efacd7c6b
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-11-27 22:05:20 +0800

    fix: tabs container

--- src/components/custom-ui/tabs/Tabs.tsx
+++ src/components/custom-ui/tabs/Tabs.tsx
@@ -16,7 +16,7 @@ const Tabs: FC<
               key={itemIndex}
               className={
                 "cursor-pointer p-2 " +
-                (isActive ? "border-b-4 border-slate-200" : "")
+                (isActive ? "border-b-4 border-slate-200" : "hover:border-b-2 border-slate-200")
               }
               onClick={() => setCurrentIndex(itemIndex)}
             >
